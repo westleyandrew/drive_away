5c9b838a932dadb5dc669ceb9b06b1aba12aa2d0
Westley Andrew <westleyandrew@hotmail.com>
2019-08-22 07:07:08 +0100

    geocoding - further work done

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,4 +1,5 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -12,8 +13,21 @@ const initMapbox = () => {
 
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
+
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+      // Create a HTML element for your custom marker
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+      // Pass the element as an argument to the new marker
+      new mapboxgl.Marker(element)
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
         .addTo(map);
     });
 
@@ -24,8 +38,9 @@ const initMapbox = () => {
     };
 
     fitMapToMarkers(map, markers);
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }));
+
   }
 };
-
 
 export { initMapbox };
