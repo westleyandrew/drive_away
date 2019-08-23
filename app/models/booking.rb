e7b0fd19545386c916e059413da8b24b8cfe0922
Westley Andrew <westleyandrew@hotmail.com>
2019-08-23 14:27:38 +0100

    booking validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,8 @@ class Booking < ApplicationRecord
   belongs_to :car
   validates :user, presence: true
   validates :car, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
 
 def start_time
